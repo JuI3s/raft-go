6f6e126eced976886c8d687efdcea057613e02b1
Julius Zhang 张瀚堃 <juliuszh@stanford.edu>
2023-02-13 21:22:52 +0800

    rpc proto

--- state/state.go
+++ state/state.go
@@ -3,17 +3,28 @@ package state
 // Persistent state on all servers
 type StatePersistent struct {
 	// latest term server has seen (initialized to 0 on first boot, increases monotonically)
-	currentTerm int
+	currentTerm uint
 
 	// candidateId that received vote in current term (or null if none)
-	votedFor int
+	votedFor uint
 
 	// log entries; each entry contains command for state machine, and term when entry was received by leader (first index is 1)
-	log []int
+	log []string
 }
 
 // Volatile state on all servers
 type StateVolatileServer struct {
-	commitIndex int
-	lastApplied int
+	// index of highest log entry known to be committed (initialized to 0, increases monotonically)
+	commitIndex uint
+
+	// index of highest log entry applied to state machine (initialized to 0, increases monotonically)
+	lastApplied uint
+}
+
+type StateVolatileLeader struct {
+	// for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
+	nextIndex []uint
+
+	// for each server, index of highest log entry known to be replicated on server (initialized to 0, increases monotonically)
+	matchIndex []uint
 }
